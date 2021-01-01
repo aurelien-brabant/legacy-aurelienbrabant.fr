efbe64a25d0ebc13a770668f66f6687fa6d00a72
abrabant <42@aurelienbrabant.fr>
2021-01-01 22:37:01 +0100

    Fix posts page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,6 +10,7 @@ import { getPostsId, getPostData } from "../../lib/posts";
 import styles from "../../styles/id.module.css";
 
 import Layout from "../../components/Layout";
+import Container from "../../components/Container";
 
 export default function Post({ postData }) {
     return (
@@ -18,26 +19,30 @@ export default function Post({ postData }) {
                 <title>{postData.title}</title>
             </Head>
             <Layout>
-                <div className={styles.wrapper}>
-                    <div
-                        className={styles.postHeader}
-                    >
-                        <h2>{postData.title}</h2>
-                        <h4>
-                            Le {" "} 
-                            {new Date(postData.date).toLocaleDateString("fr-Fr")} { " "}
-                            - Par {postData.author} 
-                        </h4>
-                        <Link href="/">
-                            <a>Retour a la liste</a>
-                        </Link>
+                <Container
+                    pageHeight
+                >
+                    <div className={styles.wrapper}>
+                        <div
+                            className={styles.postHeader}
+                        >
+                            <h2>{postData.title}</h2>
+                            <h4>
+                                Le {" "} 
+                                {new Date(postData.date).toLocaleDateString("fr-Fr")} { " "}
+                                - Par {postData.author} 
+                            </h4>
+                            <Link href="/">
+                                <a>Retour a la liste</a>
+                            </Link>
+                        </div>
+                        <div className={styles.postBody}>
+                            <Markdown
+                                markdownData={postData.content}
+                            />
+                        </div>
                     </div>
-                    <div className={styles.postBody}>
-                        <Markdown
-                            markdownData={postData.content}
-                        />
-                    </div>
-                </div>
+                </Container>
             </Layout>
         </Fragment>
     );

fa875600839ace31e32b4039cb0e5e63972b1609
abrabant <abrabant@student.42.fr>
2021-05-12 11:52:29 +0200

    Improve about page presentation

--- pages/about.js
+++ pages/about.js
@@ -8,14 +8,14 @@ export default function About() {
 		<Layout>
 			<Container
 				pageHeight
-				size="md"
+				size="lg"
 			>
 				<div className={styles.wrapper}>
 					<h1>About</h1>
 					<img src="/imgs/me.png" />
 					<div className={styles.presentation}>
 						<div>
-							<h3>Hey !</h3>
+							<h3>Hi there!</h3>
 							<p>
 								I'm Aurelien, a french programmer and student at <a href="https://42.fr/">school 42</a>.
 							</p>
@@ -26,20 +26,24 @@ export default function About() {
 						</div>
 						<div className={styles.contact}>
 							<h3>Contact</h3>
-							<h5>I'm pretty nice, don't worry :')</h5>
-							<div>
-								<a href="mailto:42@aurelienbrabant.fr">
-									42 {" "}
+							<h5>I usually answer pretty quickly!</h5>
+							<div className={styles.emailBlock}>
+								<div>
+									42 school
+								</div>
+								<a style={{display: "block"}} href="mailto:42@aurelienbrabant.fr">
 									<span className={styles.actualEmail}>
 										{" "}42@aurelienbrabant.fr
 									</span>
 								</a>
 							</div>
-							<div>
-								<a href="mailto:aurelien@aurelienbrabant.fr">
-									Pro {" "}
+							<div className={styles.emailBlock}>
+								<div>
+									Professional (only): {" "}
+								</div>
+								<a href="mailto:contact@aurelienbrabant.fr">
 									<span className={styles.actualEmail}>
-										aurelien@aurelienbrabant.fr
+										contact@aurelienbrabant.fr
 									</span>
 								</a>
 							</div>
